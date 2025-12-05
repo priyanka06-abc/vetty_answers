514e99f62c426d2a4c1662e129fcc4748abcca31
priyanka06-abc <btech10043.22@bitmesra.ac.in>
2025-12-05 19:50:48 +0530

    Update vettyans.sql

--- vettyans.sql
+++ vettyans.sql
@@ -8,3 +8,23 @@
 
 
 /* -----------------------------------------------------------
+1. Count of Purchases Per Month (Excluding Refunded purchases)
+Goal: Count transactions grouped by the month and year of the purchase, excluding any transaction that had a refund (refund_item IS NULL).
+----------------------------------------------------------- */
+
+
+SELECT
+    -- Extract Year and Month (e.g., '2019-09')
+    STRFTIME('%Y-%m', t.purchase_time) AS purchase_month,
+    COUNT(t.buyer_id) AS purchase_count
+FROM
+    transactions AS t
+WHERE
+    t.refund_item IS NULL -- Exclude refunded purchases
+GROUP BY
+    purchase_month
+ORDER BY
+    purchase_month;
+
+/* -----------------------------------------------------------
+
